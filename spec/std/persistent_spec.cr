123fea463bc1ff3f4bd0af2eb6ed7feefaee87eb
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-05-15 23:31:36 +0900

    Add spec for persistent variables

---
+++ spec/std/persistent_spec.cr
@@ -0,0 +1,14 @@
+require "spec"
+
+File.delete Persistent::FILENAME if File.exists? Persistent::FILENAME
+
+describe Persistent do
+  it "can save persistent variables" do
+    $$var = 1_i64
+    $$var.should eq(1_i64)
+    Persistent.save
+    $$var = 2_i64
+    Persistent.load
+    $$var.should eq(1_i64)
+  end
+end
